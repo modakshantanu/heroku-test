e8c0243fd4015c215005116e9dc08497a57c0899
modakshantanu <modakshantanu@gmail.com>
2019-01-22 15:46:18 +0800

    fixed port

--- bin/www.js
+++ bin/www.js
@@ -2,6 +2,6 @@ var app = require("../server/server");
 
 
 //this file is just to start listening , /server/server is imported
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("running");
 });
